56f0c61c0056d17ccd47b58c3224cd56f1b763f9
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:01:51 -0300

    SubscriptionModelAdmin.list_filter

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -6,6 +6,7 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
 	date_hierarchy = 'created_at'
 	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+	list_filter = ('created_at',)
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
