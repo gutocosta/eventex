9ab5093033714788f8aeb110f2b85e331bce57b0
Henrique Bastos <henrique@bastos.net>
2017-06-19 17:49:35 -0300

    Speaker.__str__

--- eventex/core/tests/test_model_speaker.py
+++ eventex/core/tests/test_model_speaker.py
@@ -3,8 +3,8 @@ from eventex.core.models import Speaker
 
 
 class SpeakerModelTest(TestCase):
-	def test_create(self):
-		speaker = Speaker.objects.create(
+	def setUp(self):
+		self.speaker = Speaker.objects.create(
 				name='Grace Hopper',
 				slug='grace-hopper',
 				photo='http://hbn.link/hopper-pic',
@@ -12,6 +12,7 @@ class SpeakerModelTest(TestCase):
 				description='Programadora e almirante.'
 		)
 
+	def test_create(self):
 		self.assertTrue(Speaker.objects.exists())
 
 	def test_description_can_be_blank(self):
@@ -21,3 +22,6 @@ class SpeakerModelTest(TestCase):
 	def test_website_can_be_blank(self):
 		field = Speaker._meta.get_field('website')
 		self.assertTrue(field.blank)
+
+	def test_str(self):
+		self.assertEqual('Grace Hopper', str(self.speaker))
